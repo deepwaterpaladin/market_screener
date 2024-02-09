97270539553a088b8b09d75f2207760321b0d190
Rielly Young <67346803+deepwaterpaladin@users.noreply.github.com>
2024-02-08 20:05:24 -0500

    v.0.3.1 (#5)
    * init
    
    * oops
    
    * init
    
    * update
    
    * init
    
    * added Sheet.py

--- beta/Screener.py
+++ beta/Screener.py
@@ -81,7 +81,7 @@ class Screener:
         response = requests.get(url)
         return response.json()
 
-    def __get_profile(self, ticker: str, span:int = 5) -> str:
+    def __get_profile(self, ticker: str) -> str:
         url = f'https://financialmodelingprep.com/api/v3/profile/{ticker}?apikey={self.key}'
         response = requests.get(url)
         return response.json()
@@ -288,61 +288,7 @@ class Screener:
         
         self.results = fin
         print(f"Total run time {datetime.now() - start}")
-    
-    def run_threaded(self, debug: bool = False) -> None:
-        start_time = datetime.now()
-        ticker_arr = [item for sub in self.tickers.values() for item in sub]
-        ret_dict = {i:{"Name":str, "HQ Location":str, "Has Dividends or Buybacks": bool, "Net Debt": float, "5Y average yield > 10%": bool, "Market Cap <= NCAV": bool} for i in ticker_arr}
-        steps = ['check "Has Dividends or Buybacks"', 'check "Market Cap <= NCAV"', 'check "Net Debt"', 'check "5Y average yield > 10%"','check "whitelist country"']
-        removal_matrix = [[] for i in steps]
-        threads = []
-        rs = [[],[],[],[]]
-        ret = self.__split_dict(ret_dict, 4)
-        for i in range(len(ret)):
-            thread = Thread(target=lambda one = ret[i], rs = rs[i]: removal_matrix[0].extend(self.__screen_dividends_and_buybacks(one,rs,True)))
-            threads.append(thread)
-            thread.start()
-        for thread in threads:
-            thread.join()
-        combined_dict = {}
-        for d in rs:
-            combined_dict.update(d[0])
-        ret_dict = combined_dict
-
-        ## Rest same as run(args[])
-        print(f"Time to check dividends: {datetime.now() - start_time}")
-        for i in removal_matrix[0]:
-            ret_dict.pop(i)
-    
-        if debug:
-            print(f"{len(ret_dict)} Tickers to be screened for step {steps[1]}.")
-        start_market_cap = datetime.now()
-        removal_matrix[1] = self.__screen_ncav(ret_dict, debug=debug)
-        print(f"Time to check Market Cap <= NCAV: {datetime.now() - start_market_cap}")
-        for i in removal_matrix[1]:
-            ret_dict.pop(i)
-        
-        if debug:
-            print(f"{len(ret_dict)} Tickers to be screened for step {steps[2]}.")
-        start_net_debt = datetime.now()
-        removal_matrix[2] = self.__screen_net_debt(ret_dict)
-        print(f"Time to check Net Debt: {datetime.now() - start_net_debt}")
-        for i in removal_matrix[2]:
-            ret_dict.pop(i)
-        
-        if debug:
-            print(f"{len(ret_dict)} Tickers to be screened for step {steps[3]}.")
-        start_fcf_time = datetime.now()
-        removal_matrix[3] = self.__screen_five_year_yield(ret_dict)
-        print(f"Time to check '5Y average yield > 10%': {datetime.now() - start_fcf_time}")
-        for i in removal_matrix[3]:
-            ret_dict.pop(i)
-        
-        self.results = ret_dict
-
-        print(f"Total run time {datetime.now() - start_time}")
-    
-    
+     
     def create_xlsx(self, file_path:str) -> None:
         if len(self.results) == 0:
             print(f'ERROR: results dictionary is empty. Execute `Screener.run()` to screen the stocks. If you are still seeing this after running `Screener.run()`, there are no new stocks from the previous execution.')
@@ -357,10 +303,10 @@ class Screener:
         cleaned = len(self.results)
         if debug:
             print(f"{starting_size - cleaned} tickers removed (previously present in google sheet).")
-        for k, v in self.results.items():
-            self.sheet_client.add_row_data(v)
-
+        
+        self.sheet_client.add_row_data(self.results)
         self.sheet_client.sort_values()
+        
         if debug:
             print("Google Sheet updated.")
 
--- beta/Sheet.py
+++ beta/Sheet.py
@@ -5,16 +5,59 @@
 # 
 # Sheet Name: Date
 
+import gspread
+from datetime import datetime
+import re
+
 class Sheet:
-    def __init__(self, key:str) -> None:
-        self.key = key
-        self.url = ""
+    def __init__(self) -> None:
+        self.service_account = gspread.service_account(filename="./service_account.json")
+        self.file = self.service_account.open("screener")
+        self.today = datetime.now()
+
+    def __get_worksheet_names(self) -> list[gspread.Worksheet]:
+        return self.file.worksheets()
     
+    def __extract_date_from_string(self, input_str):
+        pattern = r"'([^']+)'"
+        match = re.search(pattern, input_str)
+        if match:
+            date_str = match.group(1)
+            return date_str
+        return None
+    
+    def __add_header(self) -> None:
+        sheet = self.__get_worksheet_names()[-1]
+        sheet.append_row(values= ["Ticker", "Company Name", "Payback Rating", "NCAV Ratio", "Average Yield", "HQ Country", "Exchange Country"],table_range='A1:G1')
+
     def add_row_data(self, data: dict) -> None:
-        return NotImplementedError
+        sheet = self.__get_worksheet_names()[-1]
+        itr = 2
+        for k, v in data.items():
+            payload = [k, v['Name'], "tbd", "tbd", "tbd", v['HQ Location'], "tbd"]
+            sheet.append_row(values= payload, table_range=f'A{itr}:G{itr}')
+            itr+=1
+        print("data added to spreadsheet.")
     
     def sort_values(self) -> None:
-        return NotImplementedError
+        pass
     
-    def get_previously_seen_tickers(self) -> list[str]:
-        return NotImplementedError
+    def get_all_worksheets(self) -> list[gspread.Worksheet]:
+        return self.__get_worksheet_names()
+    
+    def create_new_tab(self) -> None:
+        try:
+            name = f"{self.today.month}-{self.today.day}-{self.today.year}"
+            self.file.add_worksheet(title = name, rows = 0, cols = 0)
+            self.__add_header()
+            print(f"Sheet {name} added.")
+        except:
+            print("Unable to add new tab. Tab already exists.")
+    
+    def get_previously_seen_tickers(self)-> list[str]:
+        today = f"{self.today.month}-{self.today.day}-{self.today.year}"
+        if today == self.__extract_date_from_string(str(self.__get_worksheet_names()[-1])):
+            most_recent_sheet = self.__extract_date_from_string(str(self.__get_worksheet_names()[-2]))
+        else:
+            most_recent_sheet = self.__extract_date_from_string(str(self.__get_worksheet_names()[-1]))
+        return [item for sublist in self.file.values_get(f"{most_recent_sheet}!A2:A100")['values'] for item in sublist]
