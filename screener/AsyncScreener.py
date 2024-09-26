d7a29aaba82201dc3a09329c63acd4f4dc882cbf
deepwaterpaladin <rielly.young11@gmail.com>
2024-09-26 07:23:35 -0400

    v.3.0.0

--- screener/AsyncScreener.py
+++ screener/AsyncScreener.py
@@ -156,6 +156,9 @@ class AsyncScreener:
         async with aiohttp.ClientSession() as session:
             async with session.get(f'https://financialmodelingprep.com/api/v4/shares_float/all?apikey={self.key}') as response:
                 return await response.json()
+            
+    def __calculate_upside(self):
+        pass
 
     async def __handle_tickers(self, tickers: list[str], debug: bool = False) -> None:
         if debug:
