6f42b758fb18ed65fff85709f828b665722cf42f
Rielly Young <rielly.young11@gmail.com>
2024-10-19 16:46:53 -0400

    v.3.5.7

--- cloud_screener.py
+++ cloud_screener.py
@@ -9,14 +9,14 @@ v2_path = './data/non_banking_tickers.json'
 test_path = './data/test_data.json'
 
 async def main() -> None:
-  a = AlphaModule(test_path, sheet_path= service_account)
-  await a.run_async(debug = True)
-  a.update_google_sheet(True)
+  a = AlphaModule(v1_path, sheet_path= service_account)
+  await a.run_async(debug= False)
+  a.update_google_sheet(debug= False)
   print("Sleeping for 1 minute.")
   sleep(60)
-  b = BetaModule(test_path, sheet_path= service_account)
-  await b.run_async(True)
-  b.update_google_sheet(True)
+  b = BetaModule(v2_path, sheet_path= service_account)
+  await b.run_async(debug= False)
+  b.update_google_sheet(debug= False)
 
 
 if __name__ == "__main__":
